02036aa877fb5df2857ef6346871d788b77cf9a9
sajaalsaab <92388761+sajaalsaab@users.noreply.github.com>
2021-11-18 20:46:27 +0300

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,24 @@
+package brick.breaker.game;
+
+/**
+ *
+ * @author sajaa
+ */
+
+import javax.swing.JFrame;
+    
+    public class MainClass {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+    
+}
+    
+ 
