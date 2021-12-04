46ca83c64a66697a892c02aefdda61a0c285388e
sajaalsaab <92388761+sajaalsaab@users.noreply.github.com>
2021-12-04 16:56:51 +0300

    Update MapGenerator.java

--- MapGenerator.java
+++ MapGenerator.java
@@ -2,7 +2,7 @@ package brick.breaker.game;
 
 
 
-    import java.awt.BasicStroke;
+    import java.awt.BasicStroke;//class for outlines of graphics primitives
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -11,9 +11,11 @@ import java.awt.Graphics2D;
  * @author chinm
  */
 public class MapGenerator {
-     public int map[][];
+     public int map[][];//contain all bricks
     public int bricksWidth;
     public int bricksHeight;
+    //argument in  class gameplay
+    //asign 1 to every array element
     public MapGenerator(int row , int col){
         map = new int[row][col];
          for (int[] map1 : map) {
@@ -21,26 +23,26 @@ public class MapGenerator {
                  map1[j] = 1;
              }
          }
-        bricksWidth = 540/col;
-        bricksHeight = 150/row;
+        bricksWidth = 640/col;
+        bricksHeight = 230/row;
     }
     public void draw(Graphics2D g) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] > 0) {
-                    g.setColor(Color.red);
-                    g.fillRect(j * bricksWidth + 80, i * bricksHeight + 50, bricksWidth, bricksHeight);
+               if (map[i][j] > 0) {//brick exist 
+                    g.setColor(new Color(255,204,229));
+                    g.fillRect(j * bricksWidth + 187, i * bricksHeight + 70, bricksWidth, bricksHeight);//fill the shape with color
 
-                    g.setStroke(new BasicStroke(3));
-                    g.setColor(Color.black);
-                    g.drawRect(j * bricksWidth + 80, i * bricksHeight + 50, bricksWidth, bricksHeight);
+                    g.setStroke(new BasicStroke(3));//border of bricks
+                    g.setColor(Color.white);
+                    g.drawRect(j * bricksWidth + 187, i * bricksHeight + 70, bricksWidth, bricksHeight);//just draw structure
 
                 }
             }
 
         }
     }
-    public void setBricksValue(int value,int row,int col)
+    public void setBricksValue(int value,int row,int col)//give a value of brick either 0 or 1 either hit ot not
     {
         map[row][col] = value;
 
