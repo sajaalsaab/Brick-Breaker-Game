a61e9ee6dbca7b7c1c01b8190d94b18c840f2b66
sajaalsaab <92388761+sajaalsaab@users.noreply.github.com>
2021-12-04 16:55:44 +0300

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -4,21 +4,24 @@ package brick.breaker.game;
  *
  * @author sajaa
  */
+// javax.swing is an library or pakcege and JFrame is class for frame
 
 import javax.swing.JFrame;
     
     public class MainClass {
     public static void main(String[] args) {
-        JFrame obj = new JFrame();
+        JFrame obj = new JFrame();// ()take string often as a title
         GamePlay gameplay = new GamePlay();
-        obj.setBounds(10,10,700,600);
-        obj.setTitle("BrickBreaker");
-        obj.setResizable(false);
-        obj.setVisible(true);
-        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        obj.add(gameplay);
+        obj.setBounds(500,100,1000,800);//obj.setBounds responsible for dimintion of frame
+// first argument for lef distince 2nd for top 3rd for width 4th for height
+        obj.setTitle("Saja's BrickBreakerGame");
+        obj.setResizable(false);// make frame able to minimize 
+        obj.setVisible(true);//setVisible to determine the window either visible or not
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//determine what will happen when click X
+        obj.add(gameplay);//add tool to frame like panel or button or text-we need method of GamePlay 
     }
     
 }
     
- 
+    
+
